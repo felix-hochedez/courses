212f82be3e5c11e8334ec49c9f64c270d37b903c
felix-hochedez <60852750+felix-hochedez@users.noreply.github.com>
2023-12-27 19:37:33 +0100

    Delete creds.py

--- creds.py
+++
@@ -1,7 +0,0 @@
-sender = 'hochedez.app@gmail.com'
-receivers = {
-    "F.H" : "hochedez1@free.fr",
-    "AS.H" : "anne-sophie@ten.com",
-    "Félix" : "felix.hochedez@gmail.com"
-}
-pwd = 'utco lpsl jfbz pdvk'
